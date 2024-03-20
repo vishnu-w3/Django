617252586211596b2a6008bc5187d15d9b509ed0
vishnu <vishnujai1429@gmail.com>
2024-03-20 21:20:08 +0530

    Changes made to Display (model) data to template

--- employees/views.py
+++ employees/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
+from django.http import Http404,HttpResponse
+
+
+from employees.models import Employee
 
 # Create your views here.
+def employee_details(request,pk):
+    # try:
+    #     employee = Employee.objects.get(pk=pk)
+    #     print(employee)
+    # except:
+    #     raise Http404
+    
+    employee = get_object_or_404(Employee,pk=pk)
+    context = {
+        'employee': employee,
+    }
+    return render(request,'employee_details.html',context)
